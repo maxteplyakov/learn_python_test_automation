1c8639ea5cda6a92569ff2e54e5990e9362b2d77
Redbu11 <bukovets@svyazcom.ru>
2021-08-23 18:48:34 +0700

    HW-16 add command line parametrs "browser" and "base_url"

--- conftest.py
+++ conftest.py
@@ -7,12 +7,14 @@ fixture = None
 @pytest.fixture (scope="session")
 def app(request):
     global fixture
+    browser = request.config.getoption('--browser')
+    base_url = request.config.getoption('--base_url')
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
         fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -24,3 +26,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='firefox')
+    parser.addoption('--base_url', action='store', default='http://localhost/addressbook/')
