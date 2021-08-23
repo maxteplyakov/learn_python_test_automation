834053d505f25827a1f2abac0833037cbe53e7ac
Redbu11 <bukovets@svyazcom.ru>
2021-08-23 20:25:28 +0700

    HW-18 add test data json generators

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,69 +1,7 @@
 # -*- coding: utf-8 -*-
-import os
-import string
-import random
 import pytest
 
 from models.contact import Contact
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters +\
-              string.digits + ' '*5        # с пробелами чаще валятся тесты
-    return prefix + ''.join([random.choice(symbols) for
-                             i in range(random.randrange(maxlen))])
-
-
-def random_email():
-    username_symbols = string.ascii_letters +\
-                       string.digits +\
-                       string.punctuation
-    domain_symbols = string.ascii_letters + string.digits
-    username = ''.join([random.choice(username_symbols) for
-                        i in range(random.randrange(20))])
-    domain = ''.join([random.choice(domain_symbols) for
-                      i in range(random.randrange(1, 20))])
-    return 'email' + username + '@' + domain + '.com'
-
-
-def random_phone_num():
-    symbols = string.digits + '+-()'
-    return ''.join([random.choice(symbols) for
-                      _ in range(random.randrange(6, 20))])
-
-
-testdata = [
-    Contact(
-        first_name='', middle_name='', last_name='',
-        nickname='', photo='',
-        title='', company='', address='',
-        home_phone_num='', cell_phone_num='', work_phone_num='',
-        fax='', email1='', email2='', email3='', homepage='',
-        bday='-', bmonth='-', byear='',
-        aday='-', amonth='-', ayear='',
-        address2='', home_phone_2='', notes=''
-    )] + [
-            Contact(
-                first_name=random_string("first_name", 20),
-                middle_name=random_string("middle_name", 20),
-                last_name=random_string("last_name", 20),
-                nickname=random_string("nickname", 20),
-                photo='../photo.jpg',
-                title=random_string("title", 20),
-                company=random_string("company", 20),
-                address=random_string("address", 50),
-                home_phone_num=random_phone_num(),
-                cell_phone_num=random_phone_num(),
-                work_phone_num=random_phone_num(),
-                fax=random_phone_num(),
-                email1=random_email(),
-                email2=random_email(),
-                email3=random_email(),
-                address2=random_string("address2", 100),
-                home_phone_2=random_phone_num(),
-                notes=random_string("notes", 30),
-            ) for _ in range(3)
-    ]
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
