de40804d58ada5f4165ca6a0070bfb4cfca038d9
Redbu11 <bukovets@svyazcom.ru>
2021-09-03 13:28:58 +0700

    HW-29 allure integration

---
+++ group.py
@@ -0,0 +1,7 @@
+============================= test session starts =============================
+platform win32 -- Python 3.8.5, pytest-6.2.4, py-1.10.0, pluggy-0.13.1
+rootdir: D:\python_learning\PDT_1st_project\learn_python_test_automation
+plugins: allure-pytest-2.9.43
+collected 0 items
+
+============================ no tests ran in 0.00s ============================
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
-import pytest
+import allure
 
 from models.group import Group
 
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('When I add a group %s to the list' % group):
+        app.group.create(group)
     # assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert old_groups == new_groups
+    with allure.step(
+            'then the new group list is equal '
+            'to the old list with the added group'
+    ):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert old_groups == new_groups
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) ==\
                sorted(app.group.get_group_list(), key=Group.id_or_max)
